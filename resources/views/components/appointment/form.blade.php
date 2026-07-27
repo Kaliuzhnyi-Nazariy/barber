ff65833c2012bba96ac1ddc64914bda2b445d49e
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-07-27 21:56:10 +0200

    feat: add styles

--- resources/views/components/appointment/form.blade.php
+++ resources/views/components/appointment/form.blade.php
@@ -1,4 +1,4 @@
-<div class="md:col-start-2">
+<div class="md:col-start-2 flex flex-col gap-1">
     <p>
         <b>Date: </b>
         <span id="chosen_date">
@@ -22,14 +22,42 @@
         <p id="total">$0 USD</p>
     </div>
 
-    <form method="POST" action="/create/appointment" class="mt-4">
+    <form method="POST" action="/create/appointment" class="mt-4 flex flex-col gap-2">
         @csrf
+        <div class="w-full flex items-center gap-2">
+            <label for="name"><b>Name: </b></label>
+            <input type="text" id="name" name="name"
+                class="outline-none w-full border-b border-b-(--dark)/50 focus-within:border-b-(--dark)"
+                placeholder="Enter your name" oninput="handleInput('name')">
+        </div>
+        <div class="w-full flex items-center gap-2">
+            <label for="email"><b>Email: </b></label>
+            <input type="email" id="email" name="email"
+                class="outline-none w-full border-b border-b-(--dark)/50 focus-within:border-b-(--dark)"
+                placeholder="Enter your email" oninput="handleInput('email')">
+        </div>
+        <div class="w-full flex items-center gap-2">
+            <label for="phone"><b>Phone: </b></label>
+            <input type="text" id="phone" name="phone"
+                class="outline-none w-full border-b border-b-(--dark)/50 focus-within:border-b-(--dark)"
+                placeholder="Enter your phone" oninput="handleInput('phone')">
+        </div>
         <input type="hidden" id="input_date" name="date">
         <input type="hidden" id="input_time" name="time">
         <input type="hidden" id="input_services" name="services">
 
         <button type="submit" id="button" disabled
-            class="w-full py-3 cursor-pointer bg-(--dark-vanila) disabled:opacity-50 disabled:cursor-not-allowed">Make
+            class="w-full py-3 cursor-pointer bg-(--dark-vanila) disabled:opacity-50 disabled:cursor-not-allowed mt-4">Make
             a reservation</button>
     </form>
+
+    @if ($errors->any())
+        <div class="bg-red-100 text-red-700 p-3 rounded mb-4">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 </div>
