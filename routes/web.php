620243413c7766946706158973b157b15a375b09
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-07-27 21:56:49 +0200

    feat: add new routes

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,9 @@ use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\ServiceController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [ServiceController::class, 'index'])->defaults('viewName', 'home');
 
-Route::get('/book/appointment', [ServiceController::class, 'index']);
-
-// Route::post('/create/appointment', [ReservationController::class, 'store']);
-
-// Route::get('/book/appointment', [ReservationController::class, 'index']);
+Route::get('/appointment', [ServiceController::class, 'index'])->defaults('viewName', 'appointment');
 
 Route::get('/api/reservations/booked-slots', [ReservationController::class, 'getBookedSlots']);
 
